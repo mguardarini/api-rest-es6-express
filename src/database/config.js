fade7f7599556ac39faa04908d31ac098513ad73
Mauro Guardarini <mauro.guardarini@gmail.com>
2018-07-01 16:02:05 -0300

    update modules

--- src/database/config.js
+++ src/database/config.js
@@ -1,8 +1,8 @@
-const host = "telemetriainstance.cjxgydrmdqll.eu-west-1.rds.amazonaws.com";
+const host = "";
 const port = 3067;
 const user = "root";
-const password = "29226818";
-const database = "telemetria";
+const password = "";
+const database = "";
 const dialect = "mysql";
 
 export default {
